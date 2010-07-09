f2a328ad46bb0c126edb933c6b5fb85cfb426cf1
Macario Ortega <macarui@gmail.com>
2010-07-09 18:05:37 -0500

    using es as a fallback locale for es-MX.

--- lib/es-mx-locale.rb
+++ lib/es-mx-locale.rb
@@ -1,5 +1,4 @@
-class MX
-  FILE = File.dirname(__FILE__)
-end
+I18n.load_path.unshift Dir.glob "#{ File.dirname(__FILE__) }/locale/*.yml"
 
-I18n.load_path += Dir.glob "#{ File.dirname(__FILE__) }/locale/*.yml"
+I18n.backend.class.send :include, I18n::Backend::Fallbacks
+I18n.fallbacks.map 'es-MX' => 'es'
